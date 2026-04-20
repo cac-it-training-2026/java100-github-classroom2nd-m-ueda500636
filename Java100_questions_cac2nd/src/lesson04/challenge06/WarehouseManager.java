9f642625151bd7797fc08401b8983dd756c0ddab
m-ueda <m-ueda@CD26303L.arksystems.co.jp>
2026-04-20 17:24:00 +0900

    lesson04 challenge07まで完了。

--- Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge06/WarehouseManager.java
@@ -39,9 +39,28 @@ public class WarehouseManager {
 
 		int[] ABKosanArray = new int[5];
 
+		//袋に入っている数値を表す変数が、intputNum。
+		int intputNum = 0;
+		boolean loopFlag = false;
 
 		//ここに重複チェックおよび値の代入処理を記述する
+		for (int i = 0; i < ABKosanArray.length; i++) {
+			do {
+				loopFlag = false;
+				intputNum = (int) (Math.random() * 10 % 5 + 1);
 
+				//trueになったら、処理を止める。	（ここでは、一度その値が入ったら止める。）			
+				for (int j = 0; j < ABKosanArray.length; j++) {
+					if (intputNum == ABKosanArray[j]) {
+						loopFlag = true;
+						break;
+
+					}
+				}
+			} while (loopFlag);
+
+			ABKosanArray[i] = intputNum;
+		}
 
 		System.out.println("E主任：");
 		System.out.println("AB興産の荷物の入れ替えをお願いします。\n");
@@ -57,11 +76,37 @@ public class WarehouseManager {
 		}
 		System.out.println("\nです。\n");
 
-
 		//ここに値の入れ替え処理を記述する
 
+		//新しいint型の配列 ABKosanArray2を設定。 配列ABKosanArrayの、1～4までの値が入った袋を、入れていく。	
+		int[] ABKosanArray2 = new int[4];
+		for (int j = 0; j < ABKosanArray2.length; j++) {
+			if (ABKosanArray[j] == 1) {
+				ABKosanArray2[0] = j;
+			} else if (ABKosanArray[j] == 2) {
+				ABKosanArray2[1] = j;
+			} else if (ABKosanArray[j] == 3) {
+				ABKosanArray2[2] = j;
+			} else if (ABKosanArray[j] == 4) {
+				ABKosanArray2[3] = j;
+			}
+
+		}
+		for (int k = 0; k < ABKosanArray2.length; k++) {
+			if (ABKosanArray2[0] == k) {
+				ABKosanArray[k] = 3;
+			} else if (ABKosanArray2[1] == k) {
+				ABKosanArray[k] = 4;
+			} else if (ABKosanArray2[2] == k) {
+				ABKosanArray[k] = 1;
+			} else if (ABKosanArray2[3] == k) {
+				ABKosanArray[k] = 2;
+			}
+
+		}
 
 		System.out.println("入れ替え後の状態は、");
+
 		for (int i = 0; i < ABKosanArray.length; i++) {
 			System.out.print(ABKosanArray[i]);
 			if (i != (ABKosanArray.length - 1)) {
